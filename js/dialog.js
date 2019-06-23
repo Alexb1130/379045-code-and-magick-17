5b0d359c70b14cd5b7ebbfd31d14f4b18a7a953a
alx <alx@alxs-MacBook-Pro.local>
2019-06-23 19:00:56 +0300

    artifacts dnd

--- js/dialog.js
+++ js/dialog.js
@@ -2,6 +2,29 @@
 
 var dialogHandler = document.querySelector('.upload');
 var dialog = document.querySelector('.setup');
+var artifactsItem = document.querySelector('.setup-artifacts-cell img');
+
+document.addEventListener('dragstart', function (evt) {
+  evt.dataTransfer.setData('text/html', 'dragstart'); // for firefox
+});
+
+document.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+});
+
+document.addEventListener('drop', function (evt) {
+  var setupArtifacts = evt.target.parentNode.classList.contains('setup-artifacts');
+  var artifactsCell = evt.target.classList.contains('setup-artifacts-cell');
+
+  if (setupArtifacts) {
+    if (artifactsCell && !artifactsCell.children) {
+      evt.preventDefault();
+      evt.target.appendChild(artifactsItem);
+    }
+  } else {
+    evt.preventDefault();
+  }
+});
 
 dialogHandler.addEventListener('mousedown', function (evt) {
   evt.preventDefault();
